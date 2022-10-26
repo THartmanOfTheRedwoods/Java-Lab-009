3d0257053394aaca1f3c735ed8ac8687bad547bf
Taro Oshiro <thebinaryduck@gmail.com>
2022-10-26 13:03:24 -0700

    Created a class to crack passwords.

--- src/Crack.java
+++ src/Crack.java
@@ -7,6 +7,7 @@ import java.io.InputStream;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.util.Arrays;
 import java.util.Scanner;
 import java.util.stream.Stream;
 
@@ -19,7 +20,20 @@ public class Crack {
         this.users = Crack.parseShadow(shadowFile);
     }
 
-    public void crack() throws FileNotFoundException {
+    public void crack(String dictPath) throws FileNotFoundException {
+        FileInputStream fis = new FileInputStream(dictPath);
+        Scanner in = new Scanner(fis);
+        while (in.hasNextLine()) {
+            String line = in.nextLine();
+            for (User u: users) {
+                if (u.getPassHash().contains("$")) {
+                    String hash = Crypt.crypt(line, u.getPassHash());
+                    if (hash.equals(u.getPassHash())) {
+                        System.out.printf("Found password %s for user %s \n", line, u.getUsername());
+                    }
+                }
+            }
+        }
     }
 
     public static int getLineCount(String path) {
@@ -31,6 +45,18 @@ public class Crack {
     }
 
     public static User[] parseShadow(String shadowFile) throws FileNotFoundException {
+        User[] users = new User[getLineCount(shadowFile)];
+        FileInputStream fis = new FileInputStream(shadowFile);
+        Scanner in = new Scanner(fis);
+        int i = 0;
+        while (in.hasNextLine()) {
+            String line = in.nextLine();
+            String[] array = line.split(":");
+            User credit = new User(array[0], array[1]);
+            users[i] = credit;
+            i++;
+        }
+        return users;
     }
 
     public static void main(String[] args) throws FileNotFoundException {
@@ -40,7 +66,8 @@ public class Crack {
         System.out.print("Type the path to your dictionary file: ");
         String dictPath = sc.nextLine();
 
+        parseShadow(shadowPath);
         Crack c = new Crack(shadowPath, dictPath);
-        c.crack();
+        c.crack(dictPath);
     }
 }
