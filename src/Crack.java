21860ae0a5abdebe3906c871c204cf6307910a9b
netlab-student <shunsinger643@gmail.com>
2022-10-26 13:16:13 -0700

    assignment done

--- src/Crack.java
+++ src/Crack.java
@@ -20,6 +20,23 @@ public class Crack {
     }
 
     public void crack() throws FileNotFoundException {
+        FileInputStream fis = new FileInputStream(dictionary);
+        Scanner s = new Scanner(fis);
+        int index = 0;
+        String[] str = new String[dictionary.length()];
+        while (s.hasNextLine()) {
+            String line = s.nextLine();
+            for (User user : users) {
+                if (user.getPassHash().contains("$")) {
+                    String hash = Crypt.crypt(line, user.getPassHash());
+                    if (hash.equals(user.getPassHash())) {
+                        System.out.println("Found password " + line + " for user " + user.getUsername());
+                    }
+                }
+            }
+            str[index] = line;
+            index++;
+        }
     }
 
     public static int getLineCount(String path) {
@@ -31,6 +48,18 @@ public class Crack {
     }
 
     public static User[] parseShadow(String shadowFile) throws FileNotFoundException {
+        User[] users = new User[getLineCount(shadowFile)];
+        FileInputStream fis = new FileInputStream(shadowFile);
+        Scanner s = new Scanner(fis);
+        int index = 0;
+        while (s.hasNextLine()) {
+            String line = s.nextLine();
+            String[] splitter = line.split(":");
+            User u = new User(splitter[0],splitter[1]);
+            users[index] = u;
+            index++;
+        }
+        return users;
     }
 
     public static void main(String[] args) throws FileNotFoundException {
